67c1057941c4af5adcada0b920968f4c7fc75f69
almog122 <almo321@gmail.com>
2021-01-18 11:31:31 +0200

    update

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    [SerializeField]
+    private float movementSpeed = 2f;
+
+    [SerializeField]
+    private GameObject player;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChasePlayer();
+
+    }
+
+    
+    private void ChasePlayer()
+    {
+        transform.LookAt(player.transform);
+        Vector3 direction = (player.transform.position - transform.position).normalized;
+        rb.MovePosition(transform.position + direction * movementSpeed * Time.deltaTime);
+    }
+
+
+}
