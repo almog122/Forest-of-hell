f475998ef378ac0e2574037ce5207120f435ead7
almog122 <almo321@gmail.com>
2021-03-15 15:39:37 +0200

    more update

--- Assets/Scripts/Sequences/HouseSeq_2.cs
+++ Assets/Scripts/Sequences/HouseSeq_2.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class HouseSeq : MonoBehaviour
+public class HouseSeq_2 : MonoBehaviour
 {
+
+    public GameObject player;
+
+    public GameObject talkTextBox;
+
+    Text textBox;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        textBox = talkTextBox.GetComponent<Text>();
     }
 
     // Update is called once per frame
--- Assets/Scripts/Sequences/OpeningSeq_1.cs
+++ Assets/Scripts/Sequences/OpeningSeq_1.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public class OpeningSeq : MonoBehaviour
+public class OpeningSeq_1 : MonoBehaviour
 {
 
     public GameObject player;
