7f1f90eded74e1e7f8723741ea61e0aaebcf1719
almog122 <almo321@gmail.com>
2021-04-19 12:52:14 +0300

    more update

---
+++ Assets/Scripts/Player/FlashLight.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour
+{
+
+    bool isOn = true;
+    public GameObject lightSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetButtonDown("Flashlight"))
+		{
+            isOn = !isOn;
+            lightSource.SetActive(isOn);
+        }
+    }
+}
